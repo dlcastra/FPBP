e999330d8accbbb65a1d7cb7c6d2993b33c99a0f
dlcastra <1dlcastra@gmail.com>
2024-05-21 14:58:00 +0200

    Refactor

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path("register/", views.register, name='register'),
+    path("register/", views.register, name="register"),
 ]

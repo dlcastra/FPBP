863688e27d277d769557e01abb5c68a8446e3e3f
dlcastra <1dlcastra@gmail.com>
2024-05-21 17:09:48 +0200

    Added model Thread

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class Thread(models.Model):
+    title = models.CharField(max_length=255)
+    context = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="threads")
+    created = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to="images/", blank=True)
+    file = models.FileField(upload_to="files/", blank=True)

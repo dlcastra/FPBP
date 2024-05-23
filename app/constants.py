fe9f3b80f73036935620803fe31a2f9278595753
dlcastra <1dlcastra@gmail.com>
2024-05-23 18:27:37 +0200

    Black refactor

--- app/constants.py
+++ app/constants.py
@@ -1,8 +1,8 @@
 PROGRAMMING_LANGUAGES = {
-            "python": "/python/",
-            "java": "/java/",
-            "javascript": "/javascript/",
-            "c": "/c/",
-            "cpp": "/cpp/",
-        }
+    "python": "/python/",
+    "java": "/java/",
+    "javascript": "/javascript/",
+    "c": "/c/",
+    "cpp": "/cpp/",
+}
 FILE_MAX_SIZE = 1024 * 1024 * 2

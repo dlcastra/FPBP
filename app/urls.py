6a74424c59134b1ea7e0aa72b00e0acf0617a8da
dlcastra <1dlcastra@gmail.com>
2024-05-24 17:05:36 +0200

    Functionality for viewing and editing a concrete track

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.MainPageView.as_view(), name="index"),
     path("threads/", views.ThreadsPageView.as_view(), name="threads"),
     path("new-thread/", views.CreateThreadView.as_view(), name="new_thread"),
+    path("thread-detail/<int:pk>", views.ThreadDetailView.as_view(), name="detail"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 
 from .constants import PROGRAMMING_LANGUAGES
 from .forms import ThreadForm
@@ -67,3 +67,52 @@ class CreateThreadView(View):
             return redirect("threads")
 
         return render(request, self.template_name, {"form": form})
+
+
+class ThreadDetailView(DetailView):
+    model = Thread
+    form_class = ThreadForm
+    template_name = "threads/threads_detail/thread_detail.html"
+
+    def get_context_data(self, request, **kwargs):
+        thread_pk = self.kwargs.get("pk")
+        thread_detail = get_object_or_404(Thread, pk=thread_pk)
+        thread = Thread.objects.filter(id=thread_pk)
+        user = request.user
+        context = {"thread_detail": thread_detail, "thread": thread, "user": user}
+
+        return context
+
+    def get_object(self, queryset=None):
+        thread_pk = self.kwargs.get("pk")
+        return get_object_or_404(Thread, pk=thread_pk)
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(request, **kwargs)
+        template = self.template_name
+
+        if "edit" in request.GET:
+            template = "threads/threads_detail/edit_thread.html"
+            form = self.form_class(instance=self.get_object())
+            return render(request, template, {"form": form})
+
+        return render(request, template, context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST, request.FILES, instance=self.get_object())
+        if form.is_valid():
+            instance = form.save(commit=False)
+            if "image" in request.FILES:
+                instance = request.FILES["image"]
+            elif "file" in request.FILES:
+                instance = request.FILES["file"]
+            instance.author = request.user
+
+            instance.save()
+            form.save()
+            return redirect('detail', pk=instance.pk)
+
+        context = self.get_context_data(request, **kwargs)
+        context['form'] = form
+        return render(request, self.template_name, context)
+

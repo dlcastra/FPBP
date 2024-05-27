20766069e7a423da5124172b0505ffbd072c1fdf
dlcastra <1dlcastra@gmail.com>
2024-05-27 16:20:31 +0200

    Thread answer problem

--- app/helpers.py
+++ app/helpers.py
@@ -8,15 +8,6 @@ def data_handler(request, pk):
     answer = ThreadAnswer.objects.filter(thread_id=pk)
     user_id = request.user.id
     feedback_html = render_to_string(
-        "threads/threads_detail/answers.html",
-        {
-            "answer": answer,
-            "csrf_token": get_token(request),
-            "user_id": user_id
-        }
+        "threads/threads_detail/answers.html", {"answer": answer, "csrf_token": get_token(request), "user_id": user_id}
     )
-    return {
-        "feedback_html": feedback_html,
-        "user_id": user_id,
-        "csrf_token": get_token(request)
-    }
+    return {"feedback_html": feedback_html, "user_id": user_id, "csrf_token": get_token(request)}

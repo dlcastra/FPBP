2b580392d342176c268cd985491088fcfdbdd448
dlcastra <1dlcastra@gmail.com>
2024-05-27 19:22:31 +0200

    Optimization and translation of functions into classes

--- app/helpers.py
+++ app/helpers.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.middleware.csrf import get_token
 from django.template.loader import render_to_string
 
@@ -12,3 +13,18 @@ def data_handler(request, pk):
         "threads/threads_detail/answers.html", {"answer": answer, "csrf_token": get_token(request), "user": user}
     )
     return {"feedback_html": feedback_html, "user_id": user_id, "csrf_token": get_token(request)}
+
+
+def post_request_threads(request, form_with_files, redirect_url):
+    form = form_with_files
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if "image" in request.FILES:
+            instance.image = request.FILES["image"]
+        elif "file" in request.FILES:
+            instance.file = request.FILES["file"]
+        instance.author = request.user
+        instance.save()
+        form.save()
+
+        return HttpResponseRedirect(redirect_url)
--- app/views.py
+++ app/views.py
@@ -1,13 +1,12 @@
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from django.views.decorators.http import require_POST
 from django.views.generic import TemplateView, DetailView
 
 from users.models import CustomUser
 from .constants import PROGRAMMING_LANGUAGES
 from .forms import ThreadForm
-from .helpers import data_handler
+from .helpers import data_handler, post_request_threads
 from .models import Thread, TutorialSection, ProgrammingLanguage, ThreadAnswer
 
 
@@ -43,7 +42,8 @@ class TutorialPageView(View):
 class ThreadsPageView(View):
     template_name = "threads/all_threads/threads_page.html"
 
-    def get_context_data(self, request):
+    @staticmethod
+    def get_context_data(request):
         threads = Thread.objects.order_by("-id")
         context = {"threads": threads}
 
@@ -63,23 +63,14 @@ class CreateThreadView(View):
 
     def get(self, request, *args, **kwargs):
         form = self.form_class(initial={"author": request.user})
-        print(request.user)
         return render(request, self.template_name, {"form": form})
 
     def post(self, request, *args, **kwargs):
-        print(request.user)
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            instance = form.save(commit=False)
-            if "image" in request.FILES:
-                instance = request.FILES["image"]
-            elif "file" in request.FILES:
-                instance = request.FILES["file"]
-            instance.author = request.user
+        form = self.form_class(request.POST, request.FILES)
+        redirect_response = post_request_threads(request, form, "/threads/")
 
-            instance.save()
-            form.save()
-            return redirect("threads")
+        if redirect_response:
+            return redirect_response
 
         return render(request, self.template_name, {"form": form})
 
@@ -92,17 +83,18 @@ class ThreadDetailView(DetailView):
     def get_context_data(self, request, **kwargs):
         user = request.user
         thread_pk = self.kwargs.get("pk")
-        thread_detail = get_object_or_404(Thread, pk=thread_pk)
         thread = Thread.objects.filter(id=thread_pk).all()
+        thread_detail = get_object_or_404(Thread, pk=thread_pk)
         answer = ThreadAnswer.objects.filter(thread_id=thread_pk).all()
+
         get_context = data_handler(self.request, thread_pk)
         get_context["thread_detail"] = thread_detail
         context = {
-            "thread_detail": thread_detail,
-            "thread": thread,
             "user": user,
-            "get_context": get_context,
+            "thread": thread,
+            "thread_detail": thread_detail,
             "answer": answer,
+            "get_context": get_context,
         }
 
         return context
@@ -116,50 +108,56 @@ class ThreadDetailView(DetailView):
         template = self.template_name
 
         if "edit" in request.GET:
-            template = "threads/threads_detail/edit_thread.html"
+            edit_template = "threads/threads_detail/edit_thread.html"
             form = self.form_class(instance=self.get_object())
-            return render(request, template, {"form": form})
+            return render(request, edit_template, {"form": form})
 
         return render(request, template, context)
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST, request.FILES, instance=self.get_object())
-        if form.is_valid():
-            instance = form.save(commit=False)
-            if "image" in request.FILES:
-                instance = request.FILES["image"]
-            elif "file" in request.FILES:
-                instance = request.FILES["file"]
-            instance.author = request.user
+        thread_pk = kwargs["pk"]
+        redirect_response = post_request_threads(request, form, f"/thread-detail/{thread_pk}")
 
-            instance.save()
-            form.save()
-            return redirect("detail", pk=instance.pk)
+        if redirect_response:
+            return redirect_response
 
         context = self.get_context_data(request, **kwargs)
         context["form"] = form
         return render(request, self.template_name, context)
 
 
-def answer_handler(request, pk):
-    get_data = data_handler(request, pk)
-    user_id = get_data["user_id"]
-    thread = get_object_or_404(Thread, id=pk)
-    if thread:
-        content = request.POST.get("feedback")
-        user = CustomUser.objects.get(id=user_id)
-        ThreadAnswer.objects.create(user=user, title=thread.title, context=content, thread=thread)
-        return JsonResponse(data_handler(request, pk))
+class AnswerHandler(View):
 
-    return JsonResponse({"message": "Invalid request"}, status=400)
+    def get_context_data(self, request, **kwargs):
+        pk = self.kwargs.get("pk")
+        get_data = data_handler(request, pk)
+        user_id = get_data["user_id"]
+        thread = get_object_or_404(Thread, pk=pk)
+
+        context = {"user_id": user_id, "thread": thread, "pk": pk}
+        return context
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(request, **kwargs)
+        thread = context["thread"]
+
+        if thread:
+            content = request.POST.get("feedback")
+            user = CustomUser.objects.get(id=context["user_id"])
+            ThreadAnswer.objects.create(user=user, title=thread.title, context=content, thread=thread)
+            return JsonResponse(data_handler(request, context["pk"]))
+
+        return JsonResponse({"message": "Invalid request"}, status=400)
 
 
-@require_POST
-def remove_answer(request, answer_id):
-    try:
-        answer = ThreadAnswer.objects.get(id=answer_id)
-        answer.delete()
+class RemoveAnswer(View):
+    def post(self, request, *args, **kwargs):
+        answer_id = self.kwargs.get("answer_id")
+        try:
+            answer = ThreadAnswer.objects.get(id=answer_id)
+            answer.delete()
 
-    except ThreadAnswer.DoesNotExist:
-        return JsonResponse({"error": "Answer does not exist"}, status=404)
-    return HttpResponse(status=204)
+        except ThreadAnswer.DoesNotExist:
+            return JsonResponse({"error": "Answer does not exist"}, status=404)
+        return HttpResponse(status=204)

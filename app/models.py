2a12c511715c7d1bc3327484d10f80b4fafc1d7e
dlcastra <1dlcastra@gmail.com>
2024-05-30 11:23:07 +0200

    Deleted model TutorialSection and added template_path field to ProgrammingLanguage

--- app/models.py
+++ app/models.py
@@ -24,19 +24,7 @@ class ThreadAnswer(models.Model):
 class ProgrammingLanguage(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
+    template_path = models.CharField(max_length=511, default="/")
 
     def __str__(self):
         return self.name
-
-
-class TutorialSection(models.Model):
-    language = models.ForeignKey(ProgrammingLanguage, on_delete=models.CASCADE, related_name="sections")
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    order = models.IntegerField()
-
-    class Meta:
-        ordering = ["order"]
-
-    def __str__(self):
-        return f"{self.language.name} - {self.title}"
